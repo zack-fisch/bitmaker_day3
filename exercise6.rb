46a3017239ed0d5867351c4009d1437ca3ca1110
Zack Fisch <zack@zfisch.com>
2014-06-03 14:30:10 -0400

    exercise 6, second commit

--- exercise6.rb
+++ exercise6.rb
@@ -1,7 +1,9 @@
 grocery_list = ["jack daniels", "baby diapers", "peanut butter"]
+grocery_list << "rice"
 grocery_list.each do |x|
 	puts "* " + x
 end
 
+puts "You have " + grocery_list.length.to_s + " items!"
 
 
