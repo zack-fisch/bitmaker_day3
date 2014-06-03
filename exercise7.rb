5de9a08bbb364219caa92457e2315efa56f5947a
Zack Fisch <zack@zfisch.com>
2014-06-03 16:30:49 -0400

    adding exercise 7

---
+++ exercise7.rb
@@ -0,0 +1,37 @@
+students = {
+	:cohort1 => 34,
+	:cohort2 => 42,
+	:cohort3 => 22
+}
+
+students[:cohort4] = 43
+
+students.each do |cohort, num| 
+	puts "#{cohort}: #{num} students"
+end
+
+
+puts "\nHash Keys:"
+puts students.keys
+
+puts "\nHash values * 0.5"
+students.values.each do |val|
+	val += (val * 0.5)
+	puts val.to_i
+end
+
+students.delete(:cohort2)
+puts students.keys
+
+#Bonus question, final jeopardy 
+
+total = 0
+
+students.values.each do |val|
+	total += val
+end
+
+puts total
+
+
+
